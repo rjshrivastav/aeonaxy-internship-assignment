4c07f0ec4e6113e14f596cc84b5697f9ddac24e6
Rjshrivastav <shrivastavranjeet.rj@gmail.com>
2024-04-07 18:34:24 +0530

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './components/signup'
-import ProfileCreation from './components/ProfileCreation'
-import Cards from './components/Cards';
-import EmailVarification from './components/EmailVarification';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './routes/signup'
+import ProfileCreation from './routes/ProfileCreation'
+import Cards from './routes/Cards';
+import EmailVarification from './routes/EmailVarification';
 
 function App() {
   return (
-    <Router basename='/aeonaxy-internship-assignment'>
       <Routes>
-        <Route path="/aeonaxy-internship-assignment" exact element={<Signup />} />
+        <Route path="/" element={<Signup />} />
         <Route path="/profilecreation" element={<ProfileCreation  />} />
         <Route path="/selectcards" element={<Cards />} />
         <Route path="/emailvarification" element={<EmailVarification />} />
       </Routes>
-    </Router>
   );
 }
 
