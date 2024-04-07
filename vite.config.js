5b5b3f46875ae3660ff1dbe1eb9f66597ae4c158
Rjshrivastav <shrivastavranjeet.rj@gmail.com>
2024-04-07 10:01:26 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/web_dev_internship_project"
+  // base: "/aeonaxy-internship-assignment"
 })
