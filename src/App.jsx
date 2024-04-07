cf0829689edf8151ba53db12d6bef56df2eea46d
Rjshrivastav <shrivastavranjeet.rj@gmail.com>
2024-04-07 11:02:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './components/signup'
-import ProfileCreation from './components/ProfileCreation'
+import Signup from './src/components/signup'
+import ProfileCreation from './src/components/ProfileCreation'
 import Cards from './components/Cards';
-import EmailVarification from './components/EmailVarification';
+import EmailVarification from './src/components/EmailVarification';
 
 function App() {
   return (
